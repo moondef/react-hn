11e6907cbbb1c7d45425c7bbb06529aa88196dd5
moondef <stpnsamko@gmail.com>
2019-06-19 18:52:16 +0300

    Content Item to hooks

--- src/features/content-item/page.js
+++ src/features/content-item/page.js
@@ -1,57 +1,49 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 
 import { rootURL } from '../../api'
 import { Header, Container, NewsItem, Comment } from '../../ui'
 
-export class ContentItem extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      points: 0,
-      user: 'username',
-      time_ago: '1 second ago',
-      url: '',
-      comments: []
-    }
+export const ContentItem = ({
+  match: {
+    params: { id }
+  }
+}) => {
+  const initData = {
+    title: '',
+    points: 0,
+    user: 'username',
+    time_ago: '1 second ago',
+    url: '',
+    comments: []
   }
 
-  componentDidMount() {
-    fetch(`${rootURL}/item/${this.props.match.params.id}`)
+  const [data, setData] = useState(initData)
+
+  useEffect(() => {
+    fetch(`${rootURL}/item/${id}`)
       .then(res => res.json())
-      .then(data =>
-        this.setState({
-          title: data.title,
-          points: data.points,
-          user: data.user,
-          time_ago: data.time_ago,
-          url: data.url,
-          comments: data.comments
-        })
-      )
-  }
+      .then(data => setData(data))
+  }, [id])
 
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>{this.state.title} | React HN</title>
-        </Helmet>
-        <Header />
-        <PageContainer>
-          <NewsItem isFullInfo {...this.state} />
-          <Comments>
-            Comments:
-            {this.state.comments.map((e, i) => (
-              <Comment {...e} comments={e.comments} key={i} />
-            ))}
-          </Comments>
-        </PageContainer>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Helmet>
+        <title>{data.title} | React HN</title>
+      </Helmet>
+      <Header />
+      <PageContainer>
+        <NewsItem isFullInfo {...data} />
+        <Comments>
+          Comments:
+          {data.comments.map((e, i) => (
+            <Comment {...e} comments={e.comments} key={i} />
+          ))}
+        </Comments>
+      </PageContainer>
+    </div>
+  )
 }
 
 const PageContainer = styled(Container)`
