19e80351c14251e76f6e2fe9a9c8dde6791ff136
moondef <stpnsamko@gmail.com>
2019-06-19 18:46:15 +0300

    ContentList to hooks

--- src/features/content-list/page.js
+++ src/features/content-list/page.js
@@ -1,106 +1,90 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Header, NewsItem, Container } from '../../ui'
 import { rootURL } from '../../api'
 
-export class ContentList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      content: [],
-      lastItemID: 0,
-      numPage: 1
-    }
-  }
+export const ContentList = ({ page }) => {
+  const initPage =
+    Number(new URL(window.location.href).searchParams.get('page')) || 1
+  const [content, setContent] = useState([])
+  const [lastItemID, setLastItemID] = useState(0)
+  const [numPage, setNumPage] = useState(initPage || 1)
 
-  componentDidMount() {
-    const num = Number(new URL(window.location.href).searchParams.get('page'))
-    fetch(`${rootURL}/${this.props.page}?page=${num || this.state.numPage}`)
+  useEffect(() => {
+    fetch(`${rootURL}/${page}?page=${numPage}`)
       .then(res => res.json())
       .then(data => {
         if (data.length) {
-          this.setState({
-            content: data,
-            lastItemID: data[data.length - 1].id,
-            numPage: num || 1
-          })
+          setContent(data)
+          setLastItemID(data[data.length - 1].id)
         }
       })
-  }
+  }, [page, numPage])
 
-  handlePagination = dir => {
+  const handlePagination = dir => {
     // // dir can be 'less' or 'more'
-    if (dir === 'more') {
-      fetch(`${rootURL}/${this.props.page}?page=${this.state.numPage + 1}`)
+    if (dir === 'next') {
+      fetch(`${rootURL}/${page}?page=${numPage + 1}`)
         .then(res => res.json())
         .then(data => {
           const dataLastItem = data[data.length - 1]
           if (dataLastItem) {
-            if (this.state.lastItemID !== dataLastItem.id) {
-              this.setState(state => ({
-                content: data,
-                lastItemID: dataLastItem.id,
-                numPage: state.numPage + 1
-              }))
+            if (lastItemID !== dataLastItem.id) {
+              setContent(data)
+              setLastItemID(dataLastItem.id)
+              setNumPage(numPage + 1)
             }
           }
         })
     } else {
-      if (this.state.numPage > 1) {
-        fetch(`${rootURL}/${this.props.page}?page=${this.state.numPage - 1}`)
+      if (numPage > 1) {
+        fetch(`${rootURL}/${page}?page=${numPage - 1}`)
           .then(res => res.json())
           .then(data => {
             const dataLastItemID = data[data.length - 1].id
-            this.setState(state => ({
-              content: data,
-              lastItemID: dataLastItemID,
-              numPage: state.numPage - 1
-            }))
+
+            setContent(data)
+            setLastItemID(dataLastItemID)
+            setNumPage(numPage - 1)
           })
       }
     }
   }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <PageContainer>
-          <div>
-            {this.state.content.map(e => (
-              <NewsItem
-                {...e}
-                key={e.id}
-                i={
-                  this.state.content.indexOf(e) +
-                  (this.state.numPage - 1) * 30 +
-                  1
-                }
-              />
-            ))}
-          </div>
+  return (
+    <div>
+      <Header />
+      <PageContainer>
+        <div>
+          {content.map(e => (
+            <NewsItem
+              {...e}
+              key={e.id}
+              i={content.indexOf(e) + (numPage - 1) * 30 + 1}
+            />
+          ))}
+        </div>
 
-          <div>
-            <Link
-              to={`?page=${this.state.numPage - 1}`}
-              onClick={() => this.handlePagination('less')}
-            >
-              {'<'}
-            </Link>
-            <span>{this.state.numPage}</span>
-            <Link
-              to={`?page=${this.state.numPage + 1}`}
-              onClick={() => this.handlePagination('more')}
-            >
-              {'>'}
-            </Link>
-          </div>
-        </PageContainer>
-      </div>
-    )
-  }
+        <div>
+          <Link
+            to={`?page=${numPage}`}
+            onClick={() => handlePagination('prev')}
+          >
+            {'<'}
+          </Link>
+          <span>{numPage}</span>
+          <Link
+            to={`?page=${numPage + 1}`}
+            onClick={() => handlePagination('next')}
+          >
+            {'>'}
+          </Link>
+        </div>
+      </PageContainer>
+    </div>
+  )
 }
 
 const PageContainer = styled(Container)`
